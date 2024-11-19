359be55d47bacaf37bea8a3f4dba2a600eb6b311
julio <juliofariadev@gmail.com>
2024-11-19 19:32:44 -0300

    feat: code style configs

--- src/server/server.ts
+++ src/server/server.ts
@@ -3,7 +3,7 @@ import express from "express";
 const server = express();
 
 server.get("/", (req, res) => {
-  return res.send("ola, DEV");
+  return res.send("Olá, DEV");
 });
 
 export { server };
